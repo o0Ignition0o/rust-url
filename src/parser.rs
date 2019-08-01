ce22785cd21cb6a64712a76e050adfe7aeee0eed
o0Ignition0o <jeremy.lempereur@gmail.com>
2019-08-01 15:44:05 +0200

    truncate at the right place.

--- src/parser.rs
+++ src/parser.rs
@@ -1139,11 +1139,7 @@ impl<'a> Parser<'a> {
                 ".." | "%2e%2e" | "%2e%2E" | "%2E%2e" | "%2E%2E" | "%2e." | "%2E." | ".%2e"
                 | ".%2E" => {
                     debug_assert!(self.serialization.as_bytes()[segment_start - 1] == b'/');
-                    if ends_with_slash {
-                        self.serialization.truncate(segment_start - 1); // Truncate "/../"
-                    } else {
-                        self.serialization.truncate(segment_start); // Truncate ".."
-                    }
+                    self.serialization.truncate(segment_start - 1); // Truncate "/.."
                     self.pop_path(scheme_type, path_start);
                     // and then if neither c is U+002F (/), nor url is special and c is U+005C (\), append the empty string to url’s path.
                     if ends_with_slash && !self.serialization.ends_with("/") {
